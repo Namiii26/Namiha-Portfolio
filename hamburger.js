34046cea35ba7c36acebfff9f51d462d3ecbeb32
Namiha173 <fathimanamiha@gmail.com>
2021-08-04 22:01:05 +0530

    first commit

---
+++ hamburger.js
@@ -0,0 +1,36 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-link");
+const navBar = document.querySelector(".nav-bar");
+const bar = document.querySelector(".bar");
+// const body = document.body;
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  navBar.classList.add("active");
+  // bar.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-Link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+  navBar.classList.remove("active");
+  // bar.classList.remove("active");
+}
+
+window.onscroll = function () {
+  if (
+    document.body.scrollTop >= 100 ||
+    document.documentElement.scrollTop >= 100
+  ) {
+    // console.log("HEllo");
+    navBar.classList.add("active");
+  } else {
+    navBar.classList.remove("active");
+  }
+};
